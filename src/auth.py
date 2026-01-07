e348fa49cb45b44d007b46884d258cf989f18d5a
wtomah <djuno@Williams-MacBook-Pro.local>
2026-01-07 09:20:29 -0600

    Hashed Passwords

--- src/auth.py
+++ src/auth.py
@@ -1,18 +1,25 @@
 import hashlib
 
 def create_user(cur):
+    hash_object = hashlib.sha256()
     print("Enter your user information")
     username = input("Username: ")
     password = input("Password: ")
     firstname = input("First Name: ")
     lastname = input("Last Name: ")
 
+    hash_object.update(password.encode('utf-8'))
+
+    hex_digest = hash_object.hexdigest()
+
     name = f"{firstname} {lastname}"
 
     cur.execute(
         "INSERT INTO login (username, password) VALUES (?, ?)",
-        (username, password)
+        (username, hex_digest)
     )
+
+    cur.connection.commit()
 
     user_id = cur.lastrowid
 
@@ -24,12 +31,18 @@ def login(cur):
     attempts = 5
 
     while attempts > 0:
+        hash_object = hashlib.sha256()
+
+
         username = input("Enter Username: ")
         password = input("Enter Password: ")
 
+        hash_object.update(password.encode('utf-8'))
+        hex_digest = hash_object.hexdigest()
+
         cur.execute(
             "SELECT id FROM login WHERE username = ? AND password = ?",
-            (username, password)
+            (username, hex_digest)
         )
 
         result = cur.fetchone()
